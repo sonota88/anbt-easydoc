dd4cf74fb3ef7eaa874549d271e3d5dd7ab627b8
user <user@squirrel.(none)>
2009-04-20 21:39:34 +0900

    use anonymous function to apply CSS

--- anbt_easydoc.js
+++ anbt_easydoc.js
@@ -67,34 +67,40 @@ foo +tt+ bar. // teletype
 
 var easyLog = function (){
   function applyDefaultCSS(){
-    var css = document.createElement("style");
-    css.innerHTML = ' \
-      * { line-height: 150%; } \
-      //body{ width: 76%; margin-left: 20%; padding: 2%; padding-top: 0; margin-top: 0; } \
-      body{ width: 80%; margin-left: 20%; padding: 0; padding-top: 0; margin-top: 0; } \
-      pre.indentBlock {  margin: 1ex 0 1ex 4ex;  padding: 0.5ex; \
-        background-color: #f0f8f8;  border: solid 1px #a0b8b8; line-height: 120%; \
-      } \
-      p.document_title { font-size: 150%; font-weight: bold; background-color: #068; color: #fff; margin: 0; margin-bottom: 1ex; padding: 2ex; text-align: center; } \
-      h1, h2, h3, h4, h5, h6, pre { margin: 0; padding: 0 1ex; } \
-      h1 { font-size: 140%; text-align: center;  border: solid #444;  border-width: 0.3ex 0;  padding: 2ex; } \
-      h2 { font-size: 130%; background-color: #ddd;  border: solid 2px #bbb;  padding: 1ex; } \
-      h3 { font-size: 120%; background-color: #eee;  padding: 1ex; } \
-      h4 { font-size: 110%; border: solid #a00; border-width: 0 0 1px 1ex  ; } \
-      h5 { font-size: 105%; border: solid #a00; border-width: 0 0 0   1ex  ; } \
-      h6 { font-size: 100%; border: solid #6a0; border-width: 0 0 0   0.5ex; } \
-      #toc { font-size: 75%; position: fixed; top: 0;left: 0; width: 18%; height: 100%; \
-        background-color: #eee; border: solid 1px #ccc; overflow: auto; padding: 1%; \
-      } \
-      #toc ul { padding-left: 2ex; } \
-      blockquote { border: solid 2px #d80; padding: 0 1ex; } \
-      div.outline{ padding: 0 0 0 2ex; } \
-      em { font-style: normal; background-color: #ff0; } \
-      tt { background-color: #ddc; } \
-      #emphasis_index { margin: 4ex 0 0 0; padding: 2ex; border: solid #000; border-width: 1px 0 0 0; } \
-    ';
-  
-    document.getElementsByTagName("head")[0].appendChild(css);
+    (function(obj){
+      var ss = document.styleSheets[0];
+      for(var selector in obj){
+        ss.insertRule(selector + "{" + obj[selector] + "}", ss.cssRules.length);
+      }
+    })({
+      "*": "line-height: 150%; -moz-border-radius: 3px;"
+    , body: "width: 80%; margin-left: 20%; padding: 0; padding-top: 0; margin-top: 0;"
+    , "pre.indentBlock": "margin: 1ex 0 1ex 4ex;  padding: 0.5ex; \
+         background-color: #f0f8f8;  border: solid 1px #a0b8b8; line-height: 120%; \
+         font-size: 90%;"
+    , "p.document_title": "font-size: 150%; font-weight: bold; \
+        background-color: #068; color: #fff; \
+        margin: 0; margin-bottom: 1ex; padding: 2ex; text-align: center;"
+    , "h1, h2, h3, h4, h5, h6, pre": "margin: 0; padding: 0 1ex;"
+    , h1: "font-size: 140%; text-align: center; \
+        background-color: #444; color: #fff;  padding: 1ex 2ex;"
+    , h2: "font-size: 130%; background-color: #ddd; \
+        border: solid 2px #bbb;  padding: 1ex;"
+    , h3: "font-size: 120%; background-color: #eee;  padding: 1ex;"
+    , h4: "font-size: 110%; border: solid #a00; border-width: 0 0 1px 1ex  ;"
+    , h5: "font-size: 105%; border: solid #a00; border-width: 0 0 0   1ex  ;"
+    , h6: "font-size: 100%; border: solid #6a0; border-width: 0 0 0   0.5ex;"
+    , "#toc": "font-size: 75%; position: fixed; \
+        top: 0;left: 0; width: 18%; height: 100%; \
+        background-color: #eee; border: solid 1px #ccc; overflow: auto; padding: 1%;"
+    , "#toc ul": "padding-left: 2ex;"
+    , blockquote: "border: solid 2px #d80; padding: 0 1ex;"
+    , "div.outline": "padding: 0 0 0 2ex;"
+    , em: "font-style: normal; background-color: #ff0;"
+    , tt: "background-color: #ddc;"
+    , "#emphasis_index": "margin: 4ex 0 0 0; padding: 2ex; \
+        border: solid #000; border-width: 1px 0 0 0;"
+    })
   }
 
 
