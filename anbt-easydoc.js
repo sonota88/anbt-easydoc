66cef713d1e85bb6eeb9a80d8dadbf453ccdae80
sonota <yosiot8753@gmail.com>
2012-02-09 00:39:20 +0900

    small fix

--- anbt-easydoc.js
+++ anbt-easydoc.js
@@ -1,13 +1,13 @@
-// anbt_easydoc.js
+// anbt-easydoc.js
 
 /* Template
 
 <html>
 <head>
-  <meta http-equiv="article-Type" article="text/html; charset=utf-8"/>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
   <link  href="prettify.css" type="text/css" rel="stylesheet">
   <script src="prettify.js" type="text/javascript"></script>
-  <script src="anbt_easydoc.js" type="text/javascript"></script>
+  <script src="anbt-easydoc.js" type="text/javascript"></script>
   <style><!-- * { line-height: 150%; } body { padding: 2ex 5%; } --></style>
 </head>
 <body onload="prettyPrint()">
@@ -67,52 +67,80 @@ foo +tt+ bar. // teletype
 
 */
 
-
 var easyLog = function (){
-  function applyDefaultCSS(){
-    (function(rules){
-      var headElement = document.getElementsByTagName("head")[0];
-      var styleElement = document.createElement("style");
-      styleElement.type = "text/css";
-      headElement.appendChild(styleElement);
 
-      var sheet = styleElement.sheet;
-      for(var selector in rules){
-        sheet.insertRule(selector + "{" + rules[selector] + "}", sheet.cssRules.length);
-      }
-    })({
-      "*": "line-height: 150%; -moz-border-radius: 3px;"
-    , body: "padding: 0; padding-top: 0; margin-top: 0;"
-    , "#document_title": "font-size: 150%; font-weight: bold; \
-        background-color: #068; color: #fff; \
-        margin: 0; margin-bottom: 1ex; padding: 2ex; text-align: center;"
-    , "#main_box": "margin: 0 0 0 20%;"
-    , "#formatted_body": "margin: 0;"
-    , "h1, h2, h3, h4, h5, h6, pre": "margin: 0; padding: 0 1ex; margin-left: -2ex;"
-    , ".outline h1, .outline h2, .outline h3, .outline h4, .outline h5, .outline h6": "margin-left: -2ex;"
-    , h1: "font-size: 140%; text-align: center; \
-        background-color: #444; color: #fff;  padding: 1ex 2ex;"
-    , h2: "font-size: 130%; background-color: #ddd; \
-        border: solid 2px #bbb;  padding: 1ex;"
-    , h3: "font-size: 120%; background-color: #eee;  padding: 1ex;"
-    , h4: "font-size: 110%; border: solid #a00; border-width: 0 0 1px 1ex  ;"
-    , h5: "font-size: 105%; border: solid #a00; border-width: 0 0 0   1ex  ;"
-    , h6: "font-size: 100%; border: solid #6a0; border-width: 0 0 0   0.5ex;"
-    , "#toc": "font-size: 75%; position: fixed; \
-        top: 0;left: 0; width: 18%; height: 100%; \
-        background-color: #eee; border: solid 1px #ccc; overflow: auto; padding: 1%;"
-    , "#toc ul": "padding-left: 2ex;"
-    , "pre.indentBlock": "margin: 1ex 0 1ex 4ex;  padding: 0.5ex; \
-         background-color: #f0f8f8;  border: solid 1px #a0b8b8; line-height: 120%; \
-         font-size: 90%;"
-    , blockquote: "border: solid 2px #d80; padding: 0 1ex;"
-    , "div.box":    "border: solid 1px #888; padding: 0 1ex;"
-    , "div.outline": "padding: 0 0 0 2ex;"
-    , em: "font-style: normal; background-color: #ff0;"
-    , tt: "background-color: #ddc;"
-    , "#emphasis_index": "margin: 4ex 0 0 0; padding: 2ex; \
-        border: solid #000; border-width: 1px 0 0 0;"
-    })
+
+  ////////////////////////////////
+  // utils
+
+  
+  function puts(){ console.log(arguments); }
+
+
+  function xtag( elem, tagName ){
+    return elem.getElementsByTagName( tagName );
+  }
+
+
+  function insertAsFirstChild(parent, child){
+    parent.insertBefore(child, parent.firstChild);
+  }
+
+
+  function applyCSSRules(rules){
+    var headElement = xtag(document, "head")[0];
+    var styleElement = document.createElement("style");
+    styleElement.type = "text/css";
+    headElement.appendChild(styleElement);
+
+    var sheet = styleElement.sheet;
+    for(var selector in rules){
+      sheet.insertRule(selector + "{" + rules[selector] + "}", sheet.cssRules.length);
+    }
+  }
+
+
+  ////////////////////////////////
+
+
+  function applyDefaultCSS(){
+    applyCSSRules({
+         "*": "line-height: 150%; -moz-border-radius: 3px;"
+         , body: "padding: 0; padding-top: 0; margin-top: 0;"
+         , "#document_title": "font-size: 150%; font-weight: bold; \
+             background-color: #068; color: #fff; \
+             margin: 0; margin-bottom: 1ex; padding: 2ex; text-align: center;"
+         , "#main_box": "margin: 0 0 0 20%;"
+         , "#formatted_body": "margin: 0;"
+         , "h1, h2, h3, h4, h5, h6, pre": "margin: 0; padding: 0 1ex; margin-left: -2ex;"
+         , ".outline h1, .outline h2, .outline h3, .outline h4, .outline h5, .outline h6": "margin-left: -2ex;"
+         , h1: "font-size: 140%; \
+             background-color: #444; color: #fff;  padding: 1ex 2ex;"
+         , h2: "font-size: 130%; background-color: #ddd; \
+             border: solid 1px #aaa;  padding: 1ex;"
+         , h3: "font-size: 120%; background-color: #eee;  padding: 1ex;  border: solid 1px #bbb;"
+         , h4: "font-size: 110%; border: solid #a00; border-width: 0 0 1px 1ex  ;"
+         , h5: "font-size: 105%; border: solid #a00; border-width: 0 0 0   1ex  ;"
+         , h6: "font-size: 100%; border: solid #6a0; border-width: 0 0 0   0.5ex;"
+         , "#toc": "font-size: 75%; position: fixed; \
+             top: 0;left: 0; width: 18%; height: 100%; \
+             background-color: #eee; border: solid 1px #ccc; overflow: auto; padding: 1%;"
+         , "#toc ul": "padding-left: 2ex;"
+         , "pre.indentBlock": "margin: 1ex 0 1ex 4ex;  padding: 0.5ex; \
+              background-color: #f4fafa;  border: solid 1px #b0c8c8; line-height: 120%; \
+              font-size: 90%;"
+         , blockquote: "border: solid 2px #d80; padding: 0 1ex;"
+         , "div.box":    "border: solid 1px #888; padding: 0 1ex;"
+         , "div.outline": "padding: 0 0 0 2ex;"
+         , "#toc a": "text-decoration: none;"
+         , em: "font-style: normal; background-color: #ff0;"
+         , tt: "background-color: #ddc;"
+         , "#emphasis_index": "margin: 4ex 0 0 0; padding: 2ex; \
+             border: solid #000; border-width: 1px 0 0 0;"
+         , "a": "color: #004477;"
+         , "a:visited": "color: #660000;"
+         , "a:hover": "color: #dd0000;"
+    });
   }
 
 
@@ -161,7 +189,7 @@ var easyLog = function (){
         c++ ; if(c>5){ break;}
       }
       return result;
-    }
+    };
     
     
     this.procPreamble = function(lines){
@@ -179,15 +207,17 @@ var easyLog = function (){
           delete lines[a];
         }
       }
-    }
+    };
     
     
     this.parse = function(text){
       var result = "";
-      var lines = text.split( "\n" )
+      var lines = text.split( "\n" );
       
       this.procPreamble(lines);
-      
+   
+      var l = null
+      , status = null;
       for(var b in lines){
         l = lines[b];
         status = null;
@@ -197,14 +227,14 @@ var easyLog = function (){
         }
         
         if( indentLineOld == false && indentLine == true ){
-          result += '<pre class="indentBlock prettyprint">'
+          result += '<pre class="indentBlock prettyprint">';
         }
         if( indentLineOld == true && indentLine == false ){
-          result += '</pre>'
+          result += '</pre>';
         }
     
         if(l.match(/^----/)){ 
-          l = "<hr />"
+          l = "<hr />";
           status = "hr";
         }else if( l.match( /^https?\:\/\// ) ){
           l = "<a href='"+ l +"'>"+ l +"</a>";
@@ -213,7 +243,7 @@ var easyLog = function (){
           l = "<a href='"+ RegExp.rightContext +"'>"+ RegExp.rightContext +"</a>";
           status = "a";
         }else if( l.match( /^img:(.+)$/ ) ){
-          l = '<img src="' + RegExp.$1 + '" />'
+          l = '<img src="' + RegExp.$1 + '" />';
           status = "img";
         }
     
@@ -254,7 +284,7 @@ var easyLog = function (){
     
           stackTOC.push(
             { "level": hLevel, "title": hTitle, "id": hId }
-          )
+          );
           status = "heading";
           outlineLevel = hLevel;
         }
@@ -278,7 +308,7 @@ var easyLog = function (){
       }
       
       return result;
-    }
+    };
     
     
     this.makeTOC = function(){
@@ -311,91 +341,96 @@ var easyLog = function (){
       }
       
       return result;
-    }
+    };
   }
 
 
   /////////////////////////////////////////////////////////////////////////////////
-  var self = this;
-	var indentLine, indentLineOld;
-	var outlineLevel = 0;
-	var outlineBeginTag = '<div class="outline">';
-	var outlineEndTag   = '</div>';
-	
-	var bodyElem = document.getElementsByTagName("body")[0]
-	var article  = document.getElementsByName("article")[0];
-	var mainBox = document.createElement("div");
-	mainBox.id = "main_box";
 
-	var parser = new Parser();
-  var result = parser.parse(article.innerHTML);
+  var indentLine, indentLineOld;
+  var outlineLevel = 0;
+  var outlineBeginTag = '<div class="outline">';
+  var outlineEndTag   = '</div>';
 
-  article.style.display = "none";
-  formatted = document.createElement("pre");
-  formatted.id = "formatted_body";
-  formatted.innerHTML = result;
-  formattedSrc = document.createElement("textarea");
-  formattedSrc.id = "formatted_body";
-  formattedSrc.innerHTML = result;
-  //document.getElementsByTagName("body")[0].insertBefore(formatted, null);
-	
-  var toc = document.createElement("div");
-  toc.id = "TOC";
-	toc.innerHTML = parser.makeTOC();
-	bodyElem.insertBefore(toc, bodyElem.firstChild);
-	
-	// Page title
-  var titleP = document.createElement("p");
-  if(self.docTitle){
-    var temp = this.docTitle;
-    if(self.by){
-      temp += " by " + self.by;
+  function main(){
+    var self = this;
+    
+    var bodyElem = document.body;
+    var article  = document.getElementsByName("article")[0];
+    var mainBox = document.createElement("div");
+    mainBox.id = "main_box";
+
+    var parser = new Parser();
+    var result = parser.parse(article.innerHTML);
+
+    article.style.display = "none";
+    var formatted = document.createElement("pre");
+    formatted.id = "formatted_body";
+    formatted.innerHTML = result;
+    var formattedSrc = document.createElement("textarea");
+    formattedSrc.id = "formatted_body";
+    formattedSrc.innerHTML = result;
+    
+    var toc = document.createElement("div");
+    toc.id = "toc";
+    toc.innerHTML = parser.makeTOC();
+    insertAsFirstChild(bodyElem, toc);
+    
+    // Page title
+    var titleP = document.createElement("p");
+    if(self.docTitle){
+      var temp = this.docTitle;
+      if(self.by){
+        temp += " by " + self.by;
+      }
+      titleP.id = "document_title";
+      titleP.innerHTML = temp;
+      
+      if(self.date){
+        temp += " (" + date + ")" ;
+      }
+      
+      document.title = temp;
     }
-    titleP.id = "document_title";
-    titleP.innerHTML = temp;
-    
-    if(self.date){
-      temp += " (" + date + ")" ;
+
+    // Index of emphatic text
+    var emReference = "";
+    var emphasis = xtag(formatted, "em");
+    var emRefElem = null;
+    if(emphasis.length > 0){
+      for(var a=0;a<emphasis.length; a++){
+        var id = "emphasis_" + a;
+        emphasis[a].id = id;
+        emReference += '<li><a href="#' + id + '">' + emphasis[a].innerHTML + '</a></li>\n';
+      }
+      emReference = "<ul>" + emReference + "</ul>";
+      emReference = "<h1>Index of emphatic texts</h1>" + emReference;
+      
+      emRefElem = document.createElement("div");
+      emRefElem.id = "emphasis_index";
+      emRefElem.innerHTML = emReference;
+      
+      // add to TOC
+      var temp = document.createElement("li");
+      temp.innerHTML = '<a href="#emphasis_index">Index of emphatic texts</a>';
+      document.getElementById("toc_list").appendChild(temp);
     }
     
-    document.title = temp;
-  }
-
-  // Index of emphatic text
-  var emReference = "";
-  var emphasis = formatted.getElementsByTagName("em");
-  var emRefElem = null;
-  if(emphasis.length > 0){
-    for(var a=0;a<emphasis.length; a++){
-      var id = "emphasis_" + a;
-      emphasis[a].id = id;
-      emReference += '<li><a href="#' + id + '">' + emphasis[a].innerHTML + '</a></li>\n'
+    if(emRefElem){
+      insertAsFirstChild(mainBox, emRefElem);
     }
-    emReference = "<ul>" + emReference + "</ul>";
-    emReference = "<h1>Index of emphatic texts</h1>" + emReference;
-    
-    emRefElem = document.createElement("div");
-    emRefElem.id = "emphasis_index";
-    emRefElem.innerHTML = emReference;
-    
-    // add to TOC
-    var temp = document.createElement("li");
-    temp.innerHTML = '<a href="#emphasis_index">Index of emphatic texts</a>';
-    document.getElementById("toc_list").appendChild(temp);
-	}
-	
-  if(emRefElem){
-    mainBox.insertBefore(emRefElem, mainBox.firstChild);
+    insertAsFirstChild(mainBox, formattedSrc);
+    insertAsFirstChild(mainBox, formatted);
+    if(self.docTitle){
+      insertAsFirstChild(mainBox, titleP);
+    }
+    insertAsFirstChild(bodyElem, mainBox);
   }
-  mainBox.insertBefore(formattedSrc, mainBox.firstChild);
-  mainBox.insertBefore(formatted, mainBox.firstChild);
-  if(self.docTitle){
-    mainBox.insertBefore(titleP, mainBox.firstChild);
-  }
-  bodyElem.insertBefore(mainBox, bodyElem.firstChild);
+  
+  main();
 
-	applyDefaultCSS();
-}
+  applyDefaultCSS();
+};
 
 
 if(navigator.userAgent.indexOf("MSIE") == -1){
